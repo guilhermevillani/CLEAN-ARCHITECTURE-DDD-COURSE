4ac1b3f0c42cc907b0680df157515c123b60586e
Guilherme Villani <guilherme170199@hotmail.com>
2024-05-22 21:23:18 -0300

    First files with the DDD architeture and JWT mocks

---
+++ BuberDinner/BuberDinner.Application/Common/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace BuberDinner.Application.Common.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(Guid userId, string firstName, string lastName);
+    }
+}
---
+++ BuberDinner/BuberDinner/Program.cs
@@ -0,0 +1,21 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    // Add services to the container.
+    builder.Services.AddApplication()
+                    .AddInfrastructure();
+
+    builder.Services.AddControllers();
+}
+
+
+
+var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
